141007e4278a3c0ab6dd5a7bca4914fd19a0d33c
Hamza Albaş <hamza.albas@hotmail.com>
2023-03-05 20:05:54 +0300

    Added a basic camera look controller.

---
+++ GA3D/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    [SerializeField] private GameObject cinemachineTargetObject;
+    private float cinemachineTargetX;
+    private float cinemachineTargetY;
+
+    public InputSystemController playerInput;
+    [SerializeField] private float minY = -30.0f;
+    [SerializeField] private float maxY = 70.0f;
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+
+    private void CameraRotation()
+    {
+        cinemachineTargetX += playerInput.lookVal.x;
+        cinemachineTargetY += playerInput.lookVal.y;
+
+        cinemachineTargetX = CameraClampAngle(cinemachineTargetX, float.MinValue, float.MaxValue);
+        cinemachineTargetY = CameraClampAngle(cinemachineTargetY, minY, maxY);
+
+        cinemachineTargetObject.transform.rotation = Quaternion.Euler(cinemachineTargetY + 0.0f, cinemachineTargetX, 0.0f);
+    }
+
+    private float CameraClampAngle(float angle, float angleMin, float angleMax)
+    {
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+
+        return Mathf.Clamp(angle, angleMin, angleMax);
+    }
+
+}
