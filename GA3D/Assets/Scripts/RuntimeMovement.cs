1b82749a9c7ea449136db7c9a69fa0320fe24f5b
Hamza Albaş <hamza.albas@hotmail.com>
2023-03-05 13:04:29 +0300

    Added a player model and animations.

--- GA3D/Assets/Scripts/RuntimeMovement.cs
+++ GA3D/Assets/Scripts/RuntimeMovement.cs
@@ -6,6 +6,8 @@ public class RuntimeMovement : MonoBehaviour
 {
     private InputSystemController input;
     private CharacterController characterController;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float animationDampTime;
 
     private void Awake()
     {
@@ -16,10 +18,17 @@ public class RuntimeMovement : MonoBehaviour
     private void Update()
     {
         Move();
+        Animate();
     }
 
     private void Move()
     {
         characterController.Move(new Vector3(input.moveVal.x * input.moveSpeed * Time.deltaTime, 0f, input.moveVal.y * input.moveSpeed * Time.deltaTime));
+    }
+
+    private void Animate()
+    {
+        animator.SetFloat("X", characterController.velocity.x, animationDampTime, Time.deltaTime);
+        animator.SetFloat("Y", characterController.velocity.z, animationDampTime, Time.deltaTime);
     }
 }
